11238d0f1b3d1810037670e076d90833e529dda7
bahstiien <bastien.lecalvez@gmail.com>
2022-09-25 23:07:48 +0200

    add slug

--- back/src/api/post/controllers/post.js
+++ back/src/api/post/controllers/post.js
@@ -1,9 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  // Method 3: Replacing a core action
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const entity = await strapi.db.query("api::post.post").findOne({
+      where: { slug: id },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
